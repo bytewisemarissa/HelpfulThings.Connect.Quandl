e8e00c730d10b41b1b27e3c34d9f9424134ec59f
Marissa <bytewisemarissa@gmail.com>
2020-01-26 02:05:45 -0500

    Fixed error unit test.

---
+++ HelpfulThings.Connect.Quandl.Tests.Live/ErrorHandlingTests.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using HelpfulThings.Connect.Quandl.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelpfulThings.Connect.Quandl.Tests.Live
+{
+    [TestClass]
+    public class ErrorHandlingTests : BaseLiveTest
+    {
+        [TestMethod]
+        public async Task Live_QuandlRequestErrorHandlingTest()
+        {
+            try
+            {
+                await TestApiClient.Database.GetDatabaseMetadata("XXXX");
+            }
+            catch (QuandlErrorException exception)
+            {
+                Assert.AreEqual("QECx02", exception.Code);
+                Assert.AreEqual("You have submitted an incorrect Quandl code. Please check your Quandl codes and try again.", exception.Message);
+            }
+        }
+    }
+}
